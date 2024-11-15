5a1ff1a1d8fe12fe0b52edad2b49ab55d69396ba
raywari <raywari31@gmail.com>
2024-11-15 08:10:25 +0300

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,24 +1,18 @@
 #!/bin/dash
-
 INPUT_FILE="domains.lst"
 OUTPUT_FILE="domains.json"
 OUTPUT_SRS_FILE="domains.srs"
-
 echo "Удаление старых файлов..."
 rm -f "$INPUT_FILE" "$OUTPUT_FILE" "$OUTPUT_SRS_FILE"
-
 URL="https://raw.githubusercontent.com/raywari/allow-domains-cidr4/refs/heads/main/domains.lst"
-
 echo "Скачивание списка доменов..."
-curl -s -o "$INPUT_FILE" "$URL"
+wget -q -O "$INPUT_FILE" "$URL"
 if [ $? -ne 0 ]; then
     echo "Ошибка при скачивании файла с доменами!"
     exit 1
 fi
-
 domain_list=""
 domain_suffix_list=""
-
 while read -r domain; do
     if echo "$domain" | grep -Eq '^[^.]+\.[^.]+$'; then
         domain_list="$domain_list\"$domain\","
@@ -26,10 +20,8 @@ while read -r domain; do
         domain_suffix_list="$domain_suffix_list\"$domain\","
     fi
 done < "$INPUT_FILE"
-
 domain_list=$(echo "$domain_list" | sed 's/,$//')
 domain_suffix_list=$(echo "$domain_suffix_list" | sed 's/,$//')
-
 echo "Создание JSON файла с правилами..."
 cat <<EOF > "$OUTPUT_FILE"
 {
@@ -48,15 +40,12 @@ cat <<EOF > "$OUTPUT_FILE"
   ]
 }
 EOF
-
 echo "JSON файл с правилами:"
 cat "$OUTPUT_FILE"
-
 echo "Компиляция в SRS файл..."
 sing-box rule-set compile "$OUTPUT_FILE" -o "$OUTPUT_SRS_FILE"
 if [ $? -ne 0 ]; then
     echo "Ошибка при компиляции .srs файла!"
     exit 1
 fi
-
 echo "Файл $OUTPUT_SRS_FILE успешно создан!"
