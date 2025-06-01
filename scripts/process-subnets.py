a72f58b4dae71360b1991a3e862a3ddc8431ddf4
raywari <raywari31@gmail.com>
2025-06-01 06:54:57 +0300

    Update process-subnets.py

--- scripts/process-subnets.py
+++ scripts/process-subnets.py
@@ -14,7 +14,7 @@ SERVICES = {
     'OVH': ('asn', 16276),
 }
 
-SUMMARY = ['Cloudflare-ECH', 'Discord', 'Meta', 'Twitter']
+SUMMARY = ['Cloudflare-ECH', 'Meta', 'Twitter']
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 BGP_URL = "https://bgp.tools/table.txt"
